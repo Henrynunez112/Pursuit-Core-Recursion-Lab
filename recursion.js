de1053b61ad81edde5103f9fa35c53fc29406121
Henrynunez112 <henrynunez@pursuit.org>
2019-11-16 17:52:59 -0500

    prob 1-3

---
+++ recursion.js
@@ -0,0 +1,24 @@
+//problem 1
+const sumRec = (num) =>{
+    if(num === 0) return 0;
+    return num += sumRec(num - 1)
+
+}
+console.log(sumRec(6))
+
+//problem 2
+
+const multArr = (arr) =>{
+    if(arr.length === 0) return 1;
+    let el = arr.pop()
+    return multArr(arr)*el
+}
+console.log(multArr([2, 3, 5]))
+
+const concatArr = (arr) =>{
+    if(arr.length === 0) return "";
+    let string = arr.concat()
+    return concatArr(string)
+}
+console.log(concatArr(['or', 'just', 'the', 'end', 'of', 'time']))
+
